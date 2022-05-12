0f8b05a3aea71646f121f0b6e705ed1591ee0320
A-Phamfam <alextampham3000@gmail.com>
2022-05-11 23:13:54 -0500

    added error logging

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+from typing import Any
 import disnake
-from disnake.ext.commands import Bot
+from disnake import ApplicationCommandInteraction
+from disnake.ext.commands import Bot, errors, Context
 from cogs import cogs_to_include
 from datetime import datetime
 from dataclasses import dataclass
 import yaml
+from util import logger
+import traceback
 
 
 @dataclass
@@ -19,10 +23,16 @@ class Keys:
     TEST_SERVER_ID: int
     TEST_SERVER_MOD_ID: int
 
+def fancy_traceback(exc: Exception) -> str:
+    """May not fit the message content limit"""
+    text = "".join(traceback.format_exception(type(exc), exc, exc.__traceback__))
+    return f"```py\n{text[-4086:]}\n```"
+
 class SATXBot(Bot):
     """
     Main discord bot instance.
     """
+
     def __init__(self, bot_prefix: str, t_keys: Keys, **settings):
         super(Bot, self).__init__(bot_prefix, **settings)
         self.keys = t_keys
@@ -34,6 +44,34 @@ class SATXBot(Bot):
     async def on_ready(self):
         print(f"{self.keys.BOT_NAME} is now ready at {datetime.now()}.\n"
               f"{self.keys.BOT_NAME} is now active in test guild {self.keys.TEST_SERVER_ID}.")
+
+    async def notify_bot_owner(self, error):
+        embed = disnake.Embed(
+            title="Error",
+            description=fancy_traceback(error),
+            color=disnake.Color.red(),
+        )
+        await self.get_user(self.keys.BOT_OWNER_ID).send(embed=embed)
+        logger.warning(fancy_traceback(error))
+
+    async def on_slash_command_error(
+            self, interaction: ApplicationCommandInteraction, exception: errors.CommandError
+    ) -> None:
+        await self.notify_bot_owner(exception)
+
+    async def on_message_command_error(self,
+                                       inter: ApplicationCommandInteraction,
+                                       error: errors.CommandError,
+                                       ) -> None:
+        await self.notify_bot_owner(error)
+
+    async def on_message_command_error(
+            self, interaction: ApplicationCommandInteraction, exception: errors.CommandError
+    ) -> None:
+        await self.notify_bot_owner(exception)
+
+    async def on_error(self, event_method: str, *args: Any, **kwargs: Any) -> None:
+        await self.notify_bot_owner()
 
 
 if __name__ == '__main__':
--- util/my_logger.py
+++ util/my_logger.py
@@ -1,6 +1,7 @@
 import logging
 from datetime import date
 from os import path, mkdir
+import disnake
 
 _log_level = "info"
 
@@ -8,7 +9,7 @@ _log_folder_name = "Logs"
 if not path.exists(_log_folder_name):
     mkdir(_log_folder_name)
 
-logger = logging.getLogger('dislash')
+logger = logging.getLogger('disnake')
 logger.setLevel(logging.INFO if _log_level == "info" else logging.DEBUG)
 _handler = logging.FileHandler(filename=f'{_log_folder_name}/{date.today()}-info.log', encoding='utf-8')
 _handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
